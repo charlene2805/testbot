3308c03d81f1f37338e9233170a9e50a2e7c641d
charlene <charlenebadibanga@gmail.com>
2017-03-27 13:40:24 +0100

    js file message change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('558816815')
 })
 
 // for Facebook verification
